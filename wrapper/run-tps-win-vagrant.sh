55cc1e0c34e26bf680cae22fc428e17441fefd13
Radek Čáp <6508800+RadekCap@users.noreply.github.com>
2023-01-30 15:48:21 +0100

    Extracted wrapper

---
+++ wrapper/run-tps-win-vagrant.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## resolve folder of this script, following all symlinks,
+## http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SCRIPT_SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPT_DIR="$(cd -P "$(dirname "$SCRIPT_SOURCE")" && pwd)"
+  SCRIPT_SOURCE="$(readlink "$SCRIPT_SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SCRIPT_SOURCE != /* ]] && SCRIPT_SOURCE="$SCRIPT_DIR/$SCRIPT_SOURCE"
+done
+SCRIPT_DIR="$(cd -P "$(dirname "$SCRIPT_SOURCE")" && pwd)"
+readonly SCRIPT_DIR
+
+set -x
+set -e
+set -o pipefail
+
+
+bash "$SCRIPT_DIR"/run-folder-as-tests.sh $SCRIPT_DIR/../tps DO_NOT_INSTALL_ANYTHING
