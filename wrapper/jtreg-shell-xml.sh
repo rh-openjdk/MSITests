12253344a154823aa50a2dcde6fffeea3f05fd41
Radek Čáp <6508800+RadekCap@users.noreply.github.com>
2023-02-07 16:04:43 +0100

    Code cleanup

--- wrapper/jtreg-shell-xml.sh
+++ wrapper/jtreg-shell-xml.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# shellcheck disable=SC2002
+
 function printXmlTest { # classname testname, time, file, jenkins view_dir
   local classname="$1"
   local testname="$2"
@@ -9,18 +12,19 @@ function printXmlTest { # classname testname, time, file, jenkins view_dir
     echo "/>"
   else
     echo ">"
-    if cat $logFile | grep -q "^\!skipped!" ; then
-      local skipMessage=`cat $logFile | grep -e   "^\!skipped!" | tail -n 1`
+    if cat "$logFile" | grep -q "^\!skipped!" ; then
+      local skipMessage
+      skipMessage=$(cat "$logFile" | grep -e   "^\!skipped!" | tail -n 1)
       echo "<skipped message=\"$skipMessage - see: $viewFileStub\"/>"
     else
       echo "      <failure message=\"see: $viewFileStub\" type=\"non zero sub-shell return code\">"
       echo -n "        <![CDATA["
       echo "----head -n 10----"
-      head -n 10 $logFile | sed "s/attribute:.\+plugin/attribute: <INVALID CHAR 05 0A>plugin/g" || true
+      head -n 10 "$logFile" | sed "s/attribute:.\+plugin/attribute: <INVALID CHAR 05 0A>plugin/g" || true
       echo "-------------- grep -n -i -e fail -e error -e \"not ok\" -B 5 -A 5--------------"
-      grep -n -i -e fail -e error -e "not ok" -B 5 -A 5 $logFile | sed "s/attribute: .\+plugin/attribute: <INVALID CHAR 05 0A>plugin/g" || true
+      grep -n -i -e fail -e error -e "not ok" -B 5 -A 5 "$logFile" | sed "s/attribute: .\+plugin/attribute: <INVALID CHAR 05 0A>plugin/g" || true
       echo "-------------- tail -n 10 --------------"
-      tail -n 10 $logFile | sed "s/attribute:.\+plugin/attribute: <INVALID CHAR 05 0A>plugin/g" || true
+      tail -n 10 "$logFile" | sed "s/attribute:.\+plugin/attribute: <INVALID CHAR 05 0A>plugin/g" || true
       echo "]]>
         </failure>" 
     fi
@@ -34,8 +38,10 @@ function printXmlHeader { # passed failed tests
   local tests="$3"
   local skipped="$4"
   local classsuite="$5"
-  local hostname=$(hostname)
-  local datetime=$(date +%Y-%m-%dT%H:%M:%S)
+  local hostname
+  hostname=$(hostname)
+  local datetime
+  datetime=$(date +%Y-%m-%dT%H:%M:%S)
   echo "<?xml version=\"1.0\"?>"
   echo "<testsuites>" 
   echo "  <testsuite errors=\"0\" failures=\"$failed\" passed=\"$passed\" tests=\"$tests\" skipped=\"$skipped\" name=\"$classsuite\" hostname=\"$hostname\" time=\"0.1\" timestamp=\"$datetime\">" #2018-03-24T22:19:45
@@ -54,8 +60,10 @@ function printNextTestsuiteStart {
   local tests="$3"
   local skipped="$4"
   local classsuite="$5"
-  local hostname=$(hostname)
-  local datetime=$(date +%Y-%m-%dT%H:%M:%S)
+  local hostname
+  hostname=$(hostname)
+  local datetime
+  datetime=$(date +%Y-%m-%dT%H:%M:%S)
   echo "  </testsuite>" 
   echo "  <testsuite errors=\"0\" failures=\"$failed\" passed=\"$passed\" tests=\"$tests\" skipped=\"$skipped\" name=\"$classsuite\" hostname=\"$hostname\" time=\"0.1\" timestamp=\"$datetime\">" #2018-03-24T22:19:45
 }
