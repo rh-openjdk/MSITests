79c3863c32328808c4bb6609f37804ab2fd342cf
Radek Čáp <6508800+RadekCap@users.noreply.github.com>
2023-02-10 13:04:06 +0100

    wip

--- wrapper/run-tps-win-vagrant.sh
+++ wrapper/run-tps-win-vagrant.sh
@@ -16,14 +16,42 @@ set -x
 set -e
 set -o pipefail
 
+# propagate RESULTS_FOLDER_NAME_INPUT for the custom path for storing tests results here
+# otherwise results forlder will be used
 export RESULTS_FOLDER_NAME="${RESULTS_FOLDER_NAME_INPUT:-results}" 
 
+# propagate JDK_VERSION_INPUT for definition which subset of tests should run
+# otherwise version 11 will be used
+export OTOOL_JDK_VERSION="${JDK_VERSION_INPUT:-11}" #todorc: implement detection of java version
+
+# propagate MSI_VENDOR_INPUT variable
+# this will ensure vendor specific parts of tests will be applied
+# otherwise RH will be used
+export MSI_VENDOR="${MSI_VENDORINPUT:-RH}"
+
+# propagate INPUT_PATH_INPUT to folder containing one msi used for tests
+# otherwise ../input forlder will be used
+export INPUT_FOLDER=$SCRIPT_DIR"${INPUT_PATH_INPUT:-/../input}"
+
+# propagate CURRENT_USER_INPUT
+# this should contains user used for testing
+# by default tester is used
+export CURRENT_USER_NAME="${CURRENT_USER_NAME_INPUT:-tester}"
+
+# propagate INSTALL_LOCATION_INPUT for the place where java will be installed
+export INSTALL_DIR_INPUT=${"INSTALL_LOCATION_INPUT:-C:\\\\Users\\\\$CURRENT_USER_NAME\\\\java"}
+
 # dependencies
-export OTOOL_jresdk=sdk
-export INPUT_FOLDER=$SCRIPT_DIR/../input
-export CURRENT_USER_NAME=tester
-export INSTALL_DIR_INPUT=C:\\\\Users\\\\$CURRENT_USER_NAME\\\\java
-export OTOOL_JDK_VERSION=11 #todorc: implement detection of java version
+export OTOOL_jresdk=sdk #hardcoded value, just sdk tests are implemented, not jre which it the other possibility
+
+
+#if [[ $OTOOL_JDK_VERSION -eq 17 ]]; then
+#  INSTALL_MODULES="jdk,jdk_registry_runtime,jdk_env_path"
+#elif [[ $OTOOL_JDK_VERSION -eq 11 ]]; then
+#  INSTALL_MODULES="jdk,jdk_registry_standard,jdk_env_path"
+#else
+#  INSTALL_MODULES="jdk,jdk_devel,jdk_registry_standard,jdk_registry_standard_devel,jdk_env_path"
+#fi
 
 # todorc: extract modules
 #export INSTALL_MODULES="jdk,jdk_registry_standard,jdk_env_path"
