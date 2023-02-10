9a0b9d7f5c09fa46a4afeef6890b09c12ab01404
Radek Čáp <6508800+RadekCap@users.noreply.github.com>
2023-02-10 13:23:18 +0100

    wip

--- wrapper/run-tps-win-vagrant.sh
+++ wrapper/run-tps-win-vagrant.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC1090,SC1091
 
 ## resolve folder of this script, following all symlinks,
 ## http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
@@ -27,6 +28,8 @@ export OTOOL_JDK_VERSION="${JDK_VERSION_INPUT:-11}" #todorc: implement detection
 # propagate MSI_VENDOR_INPUT variable
 # this will ensure vendor specific parts of tests will be applied
 # otherwise RH will be used
+# Possible implemented values are Adoptium and RH
+# You can configure specifics in configure-vendor-specific-settings.sh
 export MSI_VENDOR="${MSI_VENDORINPUT:-RH}"
 
 # propagate INPUT_PATH_INPUT to folder containing one msi used for tests
@@ -44,17 +47,8 @@ export INSTALL_DIR_INPUT=C:\\\\Users\\\\$CURRENT_USER_NAME\\\\java
 # dependencies
 export OTOOL_jresdk=sdk #hardcoded value, just sdk tests are implemented, not jre which it the other possibility
 
-
-#if [[ $OTOOL_JDK_VERSION -eq 17 ]]; then
-#  INSTALL_MODULES="jdk,jdk_registry_runtime,jdk_env_path"
-#elif [[ $OTOOL_JDK_VERSION -eq 11 ]]; then
-#  INSTALL_MODULES="jdk,jdk_registry_standard,jdk_env_path"
-#else
-#  INSTALL_MODULES="jdk,jdk_devel,jdk_registry_standard,jdk_registry_standard_devel,jdk_env_path"
-#fi
-
-# todorc: extract modules
-#export INSTALL_MODULES="jdk,jdk_registry_standard,jdk_env_path"
+source "$SCRIPT_DIR"/configure-vendor-specific-settings.sh
+setupVendorSpecific
 
 export INSTALL_LOG_FOLDER_INPUT=C:\\\\Users\\\\$CURRENT_USER_NAME\\\\install_log
 
